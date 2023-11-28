d02f7ed46e461a700bee7a157ee0989fbd2debef
Jordan Hall <j.hall@mwam.com>
2023-11-28 17:38:39 +0000

    Feat: Use noop OTEL tracer if env vars are not set
    Signed-off-by: Jordan Hall <j.hall@mwam.com>

--- kafkakewl-common/src/main/scala/com/mwam/kafkakewl/common/telemetry/GlobalTracer.scala
+++ kafkakewl-common/src/main/scala/com/mwam/kafkakewl/common/telemetry/GlobalTracer.scala
@@ -6,13 +6,22 @@
 
 package com.mwam.kafkakewl.common.telemetry
 
-import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.api.OpenTelemetry
+import io.opentelemetry.api.trace.{DefaultTracer, Tracer}
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk
-import zio.{TaskLayer, ZIO, ZLayer}
+import zio.{TaskLayer, ZIO, ZLayer, System}
 
 object GlobalTracer {
   val live: TaskLayer[Tracer] =
     ZLayer.fromZIO(
-      ZIO.attempt(AutoConfiguredOpenTelemetrySdk.initialize.getOpenTelemetrySdk.getTracer(getClass.getPackageName))
+      {
+        for {
+          _ <- System.env("OTEL_EXPORTER_OTLP_ENDPOINT").flatMap(zio.ZIO.fromOption)
+          _ <- System.env("OTEL_EXPORTER_OTLP_PROTOCOL").flatMap(zio.ZIO.fromOption)
+          _ <- System.env("OTEL_SERVICE_NAME").flatMap(zio.ZIO.fromOption)
+          _ <- System.env("OTEL_TRACES_EXPORTER").flatMap(zio.ZIO.fromOption)
+        } yield AutoConfiguredOpenTelemetrySdk.initialize.getOpenTelemetrySdk.getTracer(getClass.getPackageName)
+      }
+        .orElse(ZIO.succeed(OpenTelemetry.noop().getTracer(getClass.getPackageName)))
     )
 }
