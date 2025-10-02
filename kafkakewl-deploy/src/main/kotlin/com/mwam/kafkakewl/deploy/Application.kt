4ed5bccf636f333cc9d2199504b8fc6de41bf6d1
Laszlo Bende <L.Bende@mwam.com>
2025-10-02 12:34:30 +0100

    updating the license header year
    Signed-off-by: Laszlo Bende <L.Bende@mwam.com>

--- kafkakewl-deploy/src/main/kotlin/com/mwam/kafkakewl/deploy/Application.kt
+++ kafkakewl-deploy/src/main/kotlin/com/mwam/kafkakewl/deploy/Application.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Marshall Wace <opensource@mwam.com>
+ * SPDX-FileCopyrightText: 2025 Marshall Wace <opensource@mwam.com>
  *
  * SPDX-License-Identifier: Apache-2.0
  */
